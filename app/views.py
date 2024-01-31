08a6385fe351c04763ff4c3d863cf58c2871924a
sumi1501 <sumithramurugan1501@gmail.com>
2024-01-31 21:55:23 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def virat(request):
+    return render(request,'virat.html')
+def fans(request):
+    return render (request,'fans.html')
+def virushka(request):
+    return render (request,'virushka.html')
